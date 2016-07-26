3982ad69ba5fe42c633033da6933d19d6276e71d
Justin McGuire <jm@landedstar.com>
2016-07-26 12:45:52 -0400

    add config file

--- start.py
+++ start.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
+# load config
+
+app.config.from_pyfile('config.cfg')
+
+# test routing
+
 @app.route("/")
 def hello():
-  return "Hello World\n"
+  return "Hello World: %s\n" % app.config['SITE_NAME']
 
 @app.route("/goodbye")
 def goodbye():
@@ -14,16 +21,25 @@ def goodbye():
 def show_post(post_id):
   return "post: %d\n" % post_id
 
-if __name__ == '__main__':
-  app.run(debug=True) # auto-restart
+@app.route("/config/<string:config_var>")
+def show_config(config_var):
+  return "config_var: %r\n" % app.config[config_var]
+
+
 
 # . venv/bin/activate
 
 # to make a basic config-driven mvc cms bbq
-#  - get config
-#  - connect to db
-#  - grab first post
-#  - get template
-#  - apply post variables to template
-#  - display template
+#  - setup:
+#    - get config
+#    - connect to db
+#  - basic op
+#    - grab first post
+#    - get template
+#    - apply post variables to template
+#    - display template
+
+
+if __name__ == '__main__':
+  app.run()
 
