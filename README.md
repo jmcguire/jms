f6a1478521d498cf136bccea66b9386b2418f957
Justin McGuire <jm@landedstar.com>
2016-07-27 14:21:51 -0400

    can add posts
     - Added flash messages to all templates, which required basic session config data.
    
     - I learned that if you try to access a form variable that doesn't exist with
       `request.form['field']`, flask will fail very quietly, and without sending
       back a useful error message. I need to use `request.form.get('field', None)`
       all the time.
    
     - You can trace my "concepts learned over time" just by looking at the "from
       flask import ..." line in each commit.
    
        - And which new Jinja functions are used in the templates. I should write a
          one-liner to do that.
        - ... and two minutes later, here it is:
        - `grep -r '{%' templates/* | cut -d: -f2 | awk '{print $2}' | sort -u | grep -v "^end"`
    
     - Huge note added to README on future development. My memories of "lessons
       learned" in Perl's *Catalyst* framework are still strong.

--- README.md
+++ README.md
@@ -4,9 +4,25 @@ Get better at Python by reinventing the wheel. Specifically, a CMS.
 
 ## THE SMALL TODO
 
- - add a post
- - edit a post
- - unit testing
+  - unit testing
+
+## THE LARGE TODO
+
+In a traditional MVC, the controller runs the business logic, and decides which view to run. But this makes it difficult to unittest a controller piece without also testing the view. The business logic should not output a template. There needs to be an step inbetween the results of the business logic and the choice of view.
+
+  - Option 1: *Thick Model & Thin Controller.* Move most of the business logic to the model, and turn the Controller into a bare connection between the business logic and the view.
+
+  - Option 2: *Fourth Piece.* Add a fourth piece to MVC, inbetween the Controller and the View. It does nothing but decide, based on the returning data, which view to return to the user. So the controller would fill in any variables it needs, and the fourth piece would analyze the result. In this case, could the fourth piece be truely stateless? Just look for key pieces of data, and makes it's choice based on that?
+
+
+### Option 2:
+
+ - *router* - a simple RESTful servicer. it takes in a url and method, puts any form data on the stash, and passes control to the appropriate controller
+ - *controller* - processes any form data on the stash (not touching the request object), potentially interacting with a model, and puts result onto the stash
+ - *model* - a thin layer over the database
+ - *decider* - takes the stash data (probably, but not necessarily, put there by controller), and makes a simple decision of what view to make
+ - *view* - takes in just the data on the stash. it doesn't interact with models, but may have a read-only view of them.
+
 
 ## LICENSE
 
--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request, flash, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app = Flask(__name__)
 # load config
 
 app.config.from_pyfile('config.cfg')
+app.secret_key = app.config['SECRET_KEY']
 
 # connect to db
 
@@ -20,10 +21,10 @@ def get_db():
 def init_if_empty(force=False):
   """if the database is empty, initialize it"""
   db = get_db()
-  curr = db.cursor()
-  curr.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='table_name';")
-  row = curr.fetchone()
-  curr.close()
+  cursor = db.cursor()
+  cursor.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='table_name';")
+  row = cursor.fetchone()
+  cursor.close()
   if force or row[0] == 0:
     print "database is empty, initializing it..."
     with open(app.config['DB_INIT'], 'r') as f:
@@ -33,27 +34,69 @@ def init_if_empty(force=False):
 with app.app_context():
   init_if_empty()
 
+
 # routing
 
 @app.route("/")
 def hello():
-  return "Hello World: %s\n" % app.config['SITE_NAME']
-
-@app.route("/goodbye")
-def goodbye():
-  return "goodbye\n"
+  return "Hello and welcome to %s\n" % app.config['SITE_NAME']
 
 @app.route("/config/<string:config_var>")
 def show_config(config_var):
   return "config_var: %r\n" % app.config[config_var]
 
+
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
   db = get_db()
-  curr = db.cursor()
-  curr.execute("select * from posts where id = ?", (post_id,))
-  row = curr.fetchone()
-  return render_template('post.html', entry=row, site={'name':app.config['SITE_NAME']})
+  cursor = db.cursor()
+  cursor.execute("select * from posts where id = ?", (post_id,))
+  row = cursor.fetchone()
+  if row:
+    return render_template('post.html', entry=row, site={'name':app.config['SITE_NAME']})
+  else:
+    return render_template('post_not_found.html', post_id=post_id, site={'name':app.config['SITE_NAME']})
+    
+
+@app.route("/post")
+def show_new_post():
+  """show form to create a new post"""
+  return render_template('create_post.html', site={'name':app.config['SITE_NAME']})
+
+
+@app.route("/post", methods=['POST'])
+def create_post():
+  """create a new post from a submitted form"""
+  # get fields
+  title = request.form.get('title', None)
+  body = request.form.get('body', None)
+
+  # error check required fields
+  errors = []
+  if title is None:
+    print "one\n"
+    errors.append('Title cannot be empty')
+  if body is None:
+    print "two\n"
+    errors.append('Body cannot be empty')
+
+  if errors:
+    print "three\n"
+    for e in errors:
+      flash(e)
+    print "four\n"
+    return render_template('create_post.html', title=title, body=body, site={'name':app.config['SITE_NAME']})
+
+  # insert into database
+  db = get_db()
+  cursor = db.cursor()
+  cursor.execute("insert into posts ('title', 'body') values (?, ?)", (title, body))
+  db.commit()
+  post_id = cursor.lastrowid
+
+  # show new post to the user
+  flash("You made a new post")
+  return redirect(url_for('show_post', post_id=post_id))
 
 
 if __name__ == '__main__':
